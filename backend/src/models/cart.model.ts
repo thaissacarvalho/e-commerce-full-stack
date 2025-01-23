367dd17dae1db1b536d10c4d65887cbeffa80098
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2025-01-23 18:23:27 -0300

    new columns

--- backend/src/models/cart.model.ts
+++ backend/src/models/cart.model.ts
@@ -8,7 +8,7 @@ export interface ICart extends Document {
 const CartSchema = new Schema<ICart>({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   items: [{ productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' }, quantity: { type: Number, required: true } }],
-});
+}, { timestamps: true });
 
 const Cart = mongoose.model<ICart>('Cart', CartSchema);
 
--- backend/src/models/product.model.ts
+++ backend/src/models/product.model.ts
@@ -4,15 +4,19 @@ export interface IProduct extends Document {
   name: string;
   description: string;
   price: number;
+  stock: number;
   imageUrl: string;
+  category: string;
 }
 
 const ProductSchema = new Schema<IProduct>({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
+  stock: { type: Number, required: true, default: 0 },
   imageUrl: { type: String, required: true },
-});
+  category: { type: String, required: true}
+}, { timestamps: true });
 
 const Product = mongoose.model<IProduct>('Product', ProductSchema);
 
