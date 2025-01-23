94bd234f67a44f0a552b17b5351bc2cd7d53832f
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2025-01-23 18:22:25 -0300

    put new controllers

--- backend/src/controllers/cart.controller.ts
+++ backend/src/controllers/cart.controller.ts
@@ -1,43 +1,146 @@
 import { Request, Response } from 'express';
 import Cart from '../models/cart.model';
-import { addToCart } from '../services/cart.service';
+import { CartService } from '../services/cart.service';
 
-export const getCart = async (req: Request, res: Response) => {
-  try {
-    const cart = await Cart.findOne({ userId: req.params.userId }).populate('items.productId');
-    if (!cart) {
-      res.status(404).json({ message: 'Cart not found' });
-      return;
-    }
-    res.status(200).json(cart);
-    return;
-  } catch (error) {
-    if (error instanceof Error) {
-      if (error.message) {
+export class CartController {
+
+  private cartService: CartService;
+
+  constructor() {
+    this.cartService = new CartService();
+  }
+
+  async createCart(req: Request, res: Response) {
+    try {
+      const { userId } = req.body;
+
+      const createCart = await this.cartService.createCart(userId);
+
+      res.status(201).json(createCart);
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message) {
+          return;
+        }
+        res.status(500).json({ message: error.message });
         return;
       }
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: 'Internal server error' });
       return;
     }
-    res.status(500).json({ message: 'Internal server error' });
-    return;
   }
-};
 
-export const addItemToCart = async (req: Request, res: Response) => {
-  try {
-    const { userId, productId, quantity } = req.body;
-    const updatedCart = await addToCart(userId, productId, quantity); 
-    res.status(200).json(updatedCart);
-  } catch (error) {
-    if (error instanceof Error) {
-      if (error.message) {
+  async addItemToCart(req: Request, res: Response) {
+    try {
+      const { userId, productId, quantity } = req.body;
+      const updatedCart = await this.cartService.addToCart(userId, productId, quantity);
+      res.status(200).json(updatedCart);
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message) {
+          return;
+        }
+        res.status(500).json({ message: error.message });
         return;
       }
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: 'Internal server error' });
       return;
     }
-    res.status(500).json({ message: 'Internal server error' });
-    return;
+  };
+
+  async getCart(req: Request, res: Response) {
+    try {
+      const { userId } = req.params;
+
+      const cart = await Cart.findOne({ userId: userId }).populate('items.productId');
+      if (!cart) {
+        res.status(404).json({ message: 'Cart not found' });
+        return;
+      }
+      res.status(200).json(cart);
+      return;
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message) {
+          return;
+        }
+        res.status(500).json({ message: error.message });
+        return;
+      }
+      res.status(500).json({ message: 'Internal server error' });
+      return;
+    }
+  };
+
+  async getCartByUserId(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ message: 'ID is required' })
+        return;
+      }
+
+      const cartId = await this.cartService.getCartByUserId(id);
+
+      res.status(200).json(cartId);
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message) {
+          return;
+        }
+        res.status(500).json({ message: error.message });
+        return;
+      }
+      res.status(500).json({ message: 'Internal server error' });
+      return;
+    }
   }
-};
+  async updateCartItem(req: Request, res: Response): Promise<void> {
+    try {
+      const { userId, productId } = req.params;
+      const { quantity } = req.body;
+
+      const updatedCart = await this.cartService.updateCartItemQuantity(userId, productId, quantity);
+
+      res.status(200).json(updatedCart);
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message) {
+          res.status(401).json(error.message);
+          return;
+        }
+        res.status(500).json({ message: error.message });
+        return;
+      }
+      res.status(500).json({ message: 'Internal server error' });
+      return;
+    }
+  }
+
+  async removeFromCart(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const { productId } = req.body;
+
+      const cart = await this.cartService.removeFromCart(id, productId);
+
+      if (cart === null || cart === undefined) {
+        res.status(404).json({ error: 'Item not found' });
+        return;
+      }
+      res.status(200).json({ message: 'Item deleted successfully' });
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message) {
+          res.status(401).json(error.message);
+          return;
+        }
+        res.status(500).json({ message: error.message });
+        return;
+      }
+      res.status(500).json({ message: 'Internal server error' });
+      return;
+    }
+  }
+
+}
--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,40 +1,131 @@
 import { Request, Response } from 'express';
 import Product from '../models/product.model';
+import { ProductService } from '../services/product.service';
 
-export const getProducts = async (req: Request, res: Response) => {
-  try {
-    const products = await Product.find();
-    res.status(200).json(products);
-    return;
-  } catch (error) {
-    if (error instanceof Error) {
-      if (error.message) {
-        return;
-      }
-      res.status(500).json({ message: error.message });
-      return;
-    }
-    res.status(500).json({ message: 'Internal server error' });
-    return;
+export class ProductController {
+  private productService: ProductService; 
+
+  constructor() {
+    this.productService = new ProductService();
   }
-};
 
-export const addProduct = async (req: Request, res: Response) => {
-  try {
-    const { name, description, price, imageUrl } = req.body;
-    const newProduct = new Product({ name, description, price, imageUrl });
-    await newProduct.save();
-    res.status(201).json({ message: 'Product added successfully' });
-    return;
-  } catch (error) {
-    if (error instanceof Error) {
-      if (error.message) {
+  async addProduct(req: Request, res: Response) {
+    try {
+      const { name, description, price, stock, category, imageUrl } = req.body;
+      const newProduct = await this.productService.createProduct(name, description, price, stock, category, imageUrl);
+      await newProduct.save();
+      res.status(201).json({ message: 'Product added successfully' });
+      return;
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message) {
+          return;
+        }
+        res.status(500).json({ message: error.message });
         return;
       }
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: 'Internal server error' });
       return;
     }
-    res.status(500).json({ message: 'Internal server error' });
-    return;
+  };
+
+  async getProducts(req: Request, res: Response) {
+    try {
+      const products = await this.productService.getProducts();
+      res.status(200).json(products);
+      return;
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message) {
+          return;
+        }
+        res.status(500).json({ message: error.message });
+        return;
+      }
+      res.status(500).json({ message: 'Internal server error' });
+      return;
+    }
+  }
+
+  async getProductById(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ message: 'ID is required' })
+        return;
+      }
+
+      const productId = await this.productService.getProductById(id);
+
+      res.status(200).json(productId);
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message) {
+          return;
+        }
+        res.status(500).json({ message: error.message });
+        return;
+      }
+      res.status(500).json({ message: 'Internal server error' });
+      return;
+    }
+  }
+
+  async updateProduct(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const updates = req.body;
+
+      if (!updates || Object.keys(updates).length === 0) {
+        res.status(400).json({ message: 'No fields provided for update' });
+        return;
+      }
+
+      const updateProduct = await this.productService.updateProduct(id, updates);
+
+      if (!updateProduct) {
+        res.status(404).json({ message: 'Product not found' });
+        return;
+      }
+
+      res.status(200).json({
+        message: 'Product updated successfully',
+        product: updateProduct
+      });
+      return;
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message) {
+          res.status(401).json(error.message);
+          return; 
+        }
+        res.status(500).json({ message: error.message });
+        return; 
+      }
+      res.status(500).json({ message: 'Internal server error' });
+      return;
+    }
+  }
+
+  async deleteProduct(req: Request, res: Response): Promise<void> {
+    try {
+
+      const { id } = req.params;
+
+      const product = await this.productService.deleteProduct(id);
+      if (product === null || product === undefined) {
+        res.status(404).json({ error: 'Product not found' });
+        return;
+      }
+      res.status(200).json({ message: 'Product deleted successfully' });
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.message) {
+          res.status(401).json(error.message);
+        }
+        res.status(500).json({ message: error.message });
+      }
+      res.status(500).json({ message: 'Internal server error' });
+    }
   }
 };
