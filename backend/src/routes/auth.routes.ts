a3a5233f8ccad429f19f5ae7026aceaeaa51a1b3
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2025-01-23 18:21:31 -0300

    modified and new features for authentication

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { loginUser } from "../controllers/auth.controller";
+import { AuthController } from "../controllers/auth.controller";
 
 const router = Router();
+const authController = new AuthController();
 
-router.post('/login', loginUser);
+router.post('/login', authController.login);
--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,9 +1,7 @@
 import User from "../models/user.model";
 import jwt from 'jsonwebtoken';
 
-
 export class AuthService {
-
   async loginUser(email: string, password: string): Promise<string> {
     const jwtSecret = process.env.JWT_SECRET;
     if (!jwtSecret) throw new Error('JWT Secret is not defined');
