e3877912a1036aa7f3e477a68c66e70e905e8e45
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2025-01-26 11:07:26 -0300

    add info to deploy

--- backend/src/config/redis.config.ts
+++ backend/src/config/redis.config.ts
@@ -1,10 +1,12 @@
 import { createClient } from 'redis';
+import { app } from '..';
 
 const redis_user = process.env.REDIS_USER;
 const redis_password = process.env.REDIS_PASSWORD;
 const redis_host = process.env.REDIS_HOST;
+const PORT = process.env.PORT;
 
-export const client = createClient({
+const client = createClient({
     username: redis_user,
     password: redis_password,
     socket: {
@@ -13,10 +15,19 @@ export const client = createClient({
     }
 });
 
-client.on('error', err => console.log('Redis Client Error', err));
-
-client.connect();
+export const startServer = async () => {
+    try {
+        await client.connect();
+        console.log('Redis connected');
+        
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (err) {
+        console.log('Error connecting to Redis:', err);
+    }
+};
 
 afterAll(async () => {
-    await client.disconnect(); // Garante que a conexão seja encerrada
-  });
+    await client.disconnect();
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectToDatabase from './config/mongoose.config';
-import { client } from './config/redis.config';     
+import { startServer } from './config/redis.config';     
 import router from './routes/routes';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
@@ -19,14 +19,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-client.on('connect', () => {
-  console.log('Redis connected');
-});
+startServer();
 
 app.use('/api', router);    
 app.use('/api/v1/ecommerce/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
